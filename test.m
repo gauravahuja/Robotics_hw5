da6b2cc94091c72a18991cccf51b39d6113b6241
Gaurav Ahuja <ga2371@columbia.edu>
2014-12-06 21:26:07 -0500

    q1 working

--- test.m
+++ test.m
@@ -18,11 +18,13 @@ x_max = x_mid;
 
 max_area = size(img, 1)*size(img, 2);
 
-fwd_vel = 0.1;
-ang_vel = 0.3;
+fwd_vel = 0.2;
+ang_vel = 0.5;
 
 
 [median, initial_area] = medianObstacle(img, hsv_thresh); 
+
+min_area = 0.05*initial_area;
 
 
 while(1)
@@ -33,21 +35,26 @@ while(1)
     fwdVel = 0;
     angVel = 0;
     
-    if (area < 10)
-        fwdVel = 0;
-        angVel = 0;
-    elseif(area > initial_area)
-        fwdVel = -fwd_vel;        
-    else
-        fwdVel = fwd_vel;
-    end
-    
-    diff_x = median(2) - x_mid;
-    
-    if((abs(diff_x)) < x_max*0.1)
-        angVel = 0;
-    else
-        angVel = sign(-diff_x)*ang_vel*abs(diff_x)/x_max;
+    if (area > min_area)
+        area_ratio = area/initial_area;
+        
+        if (area_ratio <= 1.1 && area_ratio >= 0.9)
+            fwdVel = 0;
+        else
+            if(area > initial_area)
+                fwdVel = -fwd_vel;        
+            else
+                fwdVel = fwd_vel;
+            end
+        end
+
+        diff_x = median(2) - x_mid;
+
+        if((abs(diff_x)) < x_max*0.1)
+            angVel = 0;
+        else
+            angVel = sign(-diff_x)*ang_vel*abs(diff_x)/x_max;
+        end
     end
     
     fprintf('Intial Area: %f, Area: %f, fwdVel = %f, angVel = %f, diff_x = %f\n', initial_area, area, fwdVel, angVel, diff_x);
