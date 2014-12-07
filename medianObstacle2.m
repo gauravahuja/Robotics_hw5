8a40788b579ff2cea2eba86c590d4529dfc68a01
Gaurav Ahuja <ga2371@columbia.edu>
2014-12-06 22:34:29 -0500

    Fixing median obstacle 2

--- medianObstacle2.m
+++ medianObstacle2.m
@@ -14,12 +14,16 @@ function [median, area] = medianObstacle2(img, hsv)
     if h_t2 > 1
         h_t2 = h_t2 - 1;
     end
-    
+       
     if h_t2 > h_t1
         bw = img_hsv(:, :, 1) > h_t1 & img_hsv(:, :, 1) < h_t2 & img_hsv(:, :, 2) > 0.25 & img_hsv(:, :, 3) > 0.25; 
     else
-        bw = (img_hsv(:, :, 1) | h_t1 & img_hsv(:, :, 1) < h_t2) & img_hsv(:, :, 2) > 0.25 & img_hsv(:, :, 3) > 0.25; 
+        
+        bw = (img_hsv(:, :, 1) > h_t1 | img_hsv(:, :, 1) < h_t2) & img_hsv(:, :, 2) > 0.25 & img_hsv(:, :, 3) > 0.25; 
     end
+    bw = bwmorph(bw, 'erode', 4);
+    bw = bwmorph(bw, 'dilate', 2);
+    bw = bwmorph(bw, 'clean', 2);
     
     p = regionprops(bw);
     
@@ -43,6 +47,5 @@ function [median, area] = medianObstacle2(img, hsv)
         area = max_area;
         median = fliplr(max_centroid);        
     end   
-    
 end
 
